d1eb0a9a48b5f2453dc4c56f376bb1470a6ee990
Trilok Kambham <157092338+tkambham@users.noreply.github.com>
2025-02-24 11:51:08 -0600

    Time: 0 ms (100%), Space: 43.1 MB (83.31%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.java
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.java
@@ -11,18 +11,11 @@ class Solution {
                 min = nums[i];
             }
         }
-        System.out.println(min);
-        System.out.println(max);
-        if(max % min == 0){
-            return min;
-        }
-        else{
-            for(int i = min/1;i > 1; i--){
-                if(min%i == 0 && max%i==0){
-                    return i;
-                }
+        for(int i = min;i > 0; i--){
+            if(min%i == 0 && max%i==0){
+                return i;
             }
         }
-        return 1;
+        return 0;
     }
 }
