74e9c2023a9886a036a5ce99d348728acdc9bf67
Trilok Kambham <157092338+tkambham@users.noreply.github.com>
2025-08-03 11:34:33 -0500

    Time: 2 ms (64.98%), Space: 42.3 MB (72.01%) - LeetHub

---
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int[] result = new int[code.length];
+
+        if(k==0){
+            return result;
+        }
+
+        for(int i = 0;i<code.length;i++){
+            int sum =0;
+            if(k>0){
+                for(int j =1;j<=k;j++){
+                    sum += code[(i+j)%code.length];
+                }
+            }
+            else{
+                for(int j =1;j<= -k;j++){
+                    sum += code[(i-j+code.length)%code.length];
+                }
+            }
+            result[i] = sum;
+        }
+        return result;
+    }
+}
