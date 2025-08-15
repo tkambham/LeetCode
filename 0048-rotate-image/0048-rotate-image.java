0c647003bb3982c259388afbdb769f7e8ec941e9
Trilok Kambham <157092338+tkambham@users.noreply.github.com>
2025-08-15 18:10:37 -0500

    Time: 0 ms (100%), Space: 42.2 MB (58.94%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int row = matrix.length;
+        int column = matrix[0].length;
+
+        for(int i = 0;i<row; i++){
+            for(int j = i;j<column; j++){
+                if(i!=j){
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = temp;
+                }
+            }
+        }
+
+        for(int i = 0; i < row; i++){
+            for(int j=0; j < column/2; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][column-j-1];
+                matrix[i][column-j-1] = temp;
+            }
+        }
+    }
+}
