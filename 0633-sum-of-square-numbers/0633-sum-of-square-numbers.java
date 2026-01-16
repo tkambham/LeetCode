206b01f11c465c9b7fa1c54af2ebbdd2a296209b
Trilok Kambham <157092338+tkambham@users.noreply.github.com>
2026-01-15 21:46:36 -0500

    Time: 4 ms (80.62%), Space: 41.7 MB (98.52%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        long a = 0;
+        long b = (long)Math.sqrt(c);
+        while(a<=b){
+            long sum = a*a+b*b;
+            if(sum == c){
+                return true;
+            }
+            else if(sum<c){
+                a+=1;
+            }
+            else{
+                b-=1;
+            }
+        }
+        return false;
+    }
+}
